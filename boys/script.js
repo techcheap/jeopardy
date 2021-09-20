c105d788b6bf0f719cf5f833785f0bc868f5a9d6
techcheap <82733867+techcheap@users.noreply.github.com>
2021-09-20 22:07:54 +0200

    commit

---
+++ boys/script.js
@@ -0,0 +1,63 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCF8uqpoUpZ0XHIXoKZ0FYDyGX2043fqVU",
+    authDomain: "jeopardy-20f0c.firebaseapp.com",
+    databaseURL: "https://jeopardy-20f0c-default-rtdb.firebaseio.com",
+    projectId: "jeopardy-20f0c",
+    storageBucket: "jeopardy-20f0c.appspot.com",
+    messagingSenderId: "648329663372",
+    appId: "1:648329663372:web:2063cee5dfd805cc9a3ca9",
+    measurementId: "G-4W5RJRJQLD"
+};
+firebase.initializeApp(firebaseConfig)
+var firebaseRef = firebase.database();
+var myName = "hello";
+var  buzzer = {
+    gender: "boys",
+    isBuzzed: true,
+    name: myName
+};
+document.querySelector(".submit").addEventListener('click', function(){
+    myName = document.getElementById('name').value;
+    document.querySelector('.background').style.display="none";
+    buzzer = {
+        gender : "boys",
+        isBuzzed: true,
+        name: myName
+    };
+});
+firebaseRef.ref().on("value", snapshot =>{
+    var snap = snapshot.val();
+    var buzzing = snap.buzz;
+    var isBuzzed = buzzing.isBuzzed;
+    var buzzingGender = buzzing.gender;
+    var buzzingName = buzzing.name;
+    console.log(buzzingGender);
+    if (snap.onQuestion == true){
+        if (isBuzzed!= true){
+            document.querySelector('.winner').style.display="none";
+            document.querySelector('.buzzer').addEventListener('click', function(){
+                firebaseRef.ref("buzz").update(buzzer);
+            });
+        }
+        else if(isBuzzed == true){
+            var win = buzzingName + " buzzed!"
+            document.querySelector('.text').innerHTML=win;
+            document.querySelector('.winner').style.display="block";
+        };
+    }
+    else if(snap.onQuestion != true){
+    var question = "Waiting for a question...";
+    document.querySelector('.text').innerHTML=question;
+    document.querySelector('.winner').style.display="block";
+    };
+});
+
+firebaseRef.ref("timerExpired").on("value", snapshot =>{
+    snap = snapshot.val();
+    if (snap == true){
+        document.querySelector('#winner').style.display="block";
+    }
+    else if(snap== false){
+        document.querySelector('#winner').style.display="none";
+    }
+});
---
+++ host/script.js
@@ -0,0 +1,142 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCF8uqpoUpZ0XHIXoKZ0FYDyGX2043fqVU",
+    authDomain: "jeopardy-20f0c.firebaseapp.com",
+    databaseURL: "https://jeopardy-20f0c-default-rtdb.firebaseio.com",
+    projectId: "jeopardy-20f0c",
+    storageBucket: "jeopardy-20f0c.appspot.com",
+    messagingSenderId: "648329663372",
+    appId: "1:648329663372:web:2063cee5dfd805cc9a3ca9",
+    measurementId: "G-4W5RJRJQLD"
+};
+firebase.initializeApp(firebaseConfig)
+var firebaseRef = firebase.database();
+
+const deBuzz = {
+    gender:"none",
+    isBuzzed: false
+};
+var scoreBoys = 0;
+var scoreGirls = 0;
+var addScore=0;
+var winner_gender = null;
+var question ="none";
+var counting =false;
+//Trigger and close a question
+let timer = document.querySelector('.timer')
+document.querySelectorAll('.button').forEach(item => item.addEventListener("click", function() {
+    document.querySelector('.bg-question').style.display = "block";
+    document.getElementById("winner").innerHTML="";
+    timer.play();
+    endTime;
+    counting=true;
+    firebaseRef.ref("onQuestion").set(true);
+    firebaseRef.ref("buzz").update(deBuzz);
+    document.querySelector(".host-buttons").style.display="none";
+}));
+function endTime(){
+  if (counting){
+    setTimeout(firebaseRef.ref("timerExpired").set(true),10);
+  }
+};
+function rebuzz(){
+    setTimeout(function(){
+      document.getElementById("winner").innerHTML="";
+      counting = true;
+      endTime;
+      timer.play();
+      timer.currentTime=0;
+      firebaseRef.ref("onQuestion").set(true);
+      firebaseRef.ref("buzz").update(deBuzz);
+      document.querySelector(".host-buttons").style.display="none";
+    } ,10000);
+};
+document.querySelector('.close').addEventListener("click", function(){
+    document.querySelector('.bg-question').style.display = "none";
+    timer.pause();
+    timer.currentTime=0;
+    firebaseRef.ref("buzz/isBuzzed").set(false);
+    firebaseRef.ref("onQuestion").set(false);
+    firebaseRef.ref("timerExpired").set(false);
+    document.getElementById(question).className="clicked";
+});
+
+firebaseRef.ref("buzz").on("value", snapshot =>{
+    var snap = snapshot.val();
+    var isBuzzed = snap.isBuzzed;
+    if (isBuzzed == true){
+      rebuzz;
+      counting = false;
+      document.getElementById("winner").innerHTML=snap.name + " buzzed!";
+      timer.currentTime=0;
+      document.querySelector(".host-buttons").style.display="block";
+    }
+});
+
+function reply_click(clicked_id)
+{
+  question = clicked_id;
+  addScore = parseInt(clicked_id, 10);
+  firebaseRef.ref("questions").on("value", snapshot =>{
+    snap = snapshot.val();
+    var myQuestion = "none";
+    switch (question.toString()){
+      case '100-1':
+        myQuestion = snap.first1.toString();
+        break;
+      case '100-2':
+        myQuestion = snap.second1.toString();
+        break;
+      case '200-1':
+        myQuestion = snap.first2.toString();
+        break;
+      case '200-2':
+        myQuestion = snap.second2.toString();
+        break;
+      case '300-1':
+        myQuestion = snap.first3.toString();
+        break;
+      case '300-2':
+        myQuestion = snap.second3.toString();
+        break;
+      case '400-1':
+        myQuestion = snap.first4.toString();
+        break;
+      case '400-2':
+        myQuestion = snap.second4.toString();
+        break;
+      case '500-1':
+        myQuestion = snap.first5.toString();
+        break;
+      case '500-2':
+        myQuestion = snap.second5.toString();
+        break;
+    };
+    document.getElementById("question").innerHTML=myQuestion;
+  });
+};
+
+
+firebaseRef.ref("buzz/gender").on("value", snapshot =>{
+    var winner = snapshot.val();
+    if (winner.toString() == "boys"){
+      winner_gender=true;
+    }
+    else if (winner.toString() == "girls"){
+      winner_gender=false;
+    }
+});
+
+document.getElementById("correct").addEventListener("click", function(){
+  counting = false;
+  if (winner_gender == true){
+    scoreBoys = scoreBoys + addScore;
+  }else if (winner_gender == false){
+    scoreGirls = scoreGirls + addScore;
+  }
+  timer.pause();
+  document.querySelector(".host-buttons").style.display="none";
+  var scoreboys = "Boys: " + scoreBoys + "pts";
+  var scoregirls = "Girls: " + scoreGirls + "pts";
+  document.getElementById("boys").innerHTML = scoreboys;
+  document.getElementById("girls").innerHTML = scoregirls;
+});
